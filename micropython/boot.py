0eb2ec5192bda93c00ddb95787268d1d4e88028b
Caue Baasch de Souza <cauebaasch@gmail.com>
2017-09-28 20:04:25 -0300

    Initial commit

---
+++ micropython/boot.py
@@ -0,0 +1,21 @@
+import gc
+from network import WLAN, AP_IF, STA_IF
+import webrepl
+
+
+AP_WLAN = WLAN(AP_IF)
+STA_WLAN = WLAN(STA_IF)
+
+# AP_WLAN.active(True)
+# AP_WLAN.config(essid='pyoak2', password='deprecated')
+
+STA_WLAN.active(True)
+STA_WLAN.connect('caravela', 'deprecated')
+while not STA_WLAN.isconnected():
+    pass
+
+webrepl.start()
+
+del WLAN, AP_IF, STA_IF, webrepl
+gc.collect()
+del gc
---
+++ micropython/pysh.py
@@ -0,0 +1,31 @@
+import os
+
+
+def ls(path='.'):
+    out = os.listdir(path)
+    out = '\n'.join(out)
+    print(out)
+
+
+def cat(file_name):
+    with open(file_name) as f:
+        print(f.read())
+
+
+def pwd():
+    print(os.getcwd())
+
+
+def cd(path):
+    os.chdir(path)
+
+
+def rm(*args):
+    for path in args:
+        try:
+            os.remove(path)
+        except OSError:
+            if '-r' in args:
+                os.rmdir(path)
+            else:
+                raise OSError(path + 'is a directory')
