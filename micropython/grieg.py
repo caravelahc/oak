d984e0b6c179093681b8332d380daedc906d7a07
Caue Baasch de Souza <cauebaasch@gmail.com>
2017-09-29 18:09:26 -0300

    Added some more Grieg

--- micropython/grieg.py
+++ micropython/grieg.py
@@ -1,51 +1,87 @@
 from music import Note, Pitch
+from music import Buzzer, BuzzerBand
+import oak
+
+
+def main():
+    b1 = Buzzer(oak.PINS[5])
+    b2 = Buzzer(oak.PINS[6])
+
+    band = BuzzerBand(b1, b2)
+    band.play_tunes(IN_THE_HALL_OF_THE_MOUNTAIN_KING_1,
+                    IN_THE_HALL_OF_THE_MOUNTAIN_KING_2, speed=2)
+
+
+IN_THE_HALL_OF_THE_MOUNTAIN_KING_1 = (
+    Note(Pitch.E[3], 10, 4, 0),
+
+    Note(Pitch.A[1], 10, 0.5, 1), Note(Pitch.B[1], 10, 0.5, 1),
+    Note(Pitch.C[2], 10, 0.5, 1), Note(Pitch.D[2], 10, 0.5, 1),
+    Note(Pitch.E[2], 20, 0.5, 1), Note(Pitch.C[2], 10, 0.5, 1),
+    Note(Pitch.E[2], 10, 1, 1),
+
+    Note(Pitch.Eb[2], 20, 0.5, 1), Note(Pitch.B[1], 10, 0.5, 1),
+    Note(Pitch.Eb[2], 10, 1, 1),
+    Note(Pitch.D[2], 20, 0.5, 1), Note(Pitch.Bb[2], 10, 0.5, 1),
+    Note(Pitch.D[2], 10, 1, 1),
+
+    Note(Pitch.A[1], 10, 0.5, 1), Note(Pitch.B[1], 10, 0.5, 1),
+    Note(Pitch.C[2], 10, 0.5, 1), Note(Pitch.D[2], 10, 0.5, 1),
+    Note(Pitch.E[2], 20, 0.5, 1), Note(Pitch.C[2], 10, 0.5, 1),
+    Note(Pitch.E[2], 10, 0.5, 1), Note(Pitch.A[2], 10, 0.5, 1),
+
+    Note(Pitch.G[2], 10, 0.5, 1), Note(Pitch.E[2], 10, 0.5, 1),
+    Note(Pitch.C[2], 20, 0.5, 1), Note(Pitch.E[2], 10, 0.5, 1),
+    Note(Pitch.G[2], 10, 2, 0),
+    # ...
+)
 
 IN_THE_HALL_OF_THE_MOUNTAIN_KING_2 = (
-    Note(Pitch.E[2], 5, 4),
+    Note(Pitch.E[2], 5, 4, 0),
 
-    Note(Pitch.A[0], 5, 1), Note(Pitch.E[1], 5, 1),
-    Note(Pitch.A[0], 5, 1), Note(Pitch.E[1], 5, 1),
+    Note(Pitch.A[0], 5, 1, 1), Note(Pitch.E[1], 5, 1, 1),
+    Note(Pitch.A[0], 5, 1, 1), Note(Pitch.E[1], 5, 1, 1),
 
-    Note(Pitch.A[0], 5, 1), Note(Pitch.E[1], 5, 1),
-    Note(Pitch.A[0], 5, 1), Note(Pitch.E[1], 5, 1),
+    Note(Pitch.A[0], 5, 1, 1), Note(Pitch.E[1], 5, 1, 1),
+    Note(Pitch.A[0], 5, 1, 1), Note(Pitch.E[1], 5, 1, 1),
 
-    Note(Pitch.A[0], 5, 1), Note(Pitch.E[1], 5, 1),
-    Note(Pitch.A[0], 5, 1), Note(Pitch.E[1], 5, 1),
+    Note(Pitch.A[0], 5, 1, 1), Note(Pitch.E[1], 5, 1, 1),
+    Note(Pitch.A[0], 5, 1, 1), Note(Pitch.E[1], 5, 1, 1),
 
-    Note(Pitch.C[1], 5, 1), Note(Pitch.G[1], 5, 1),
-    Note(Pitch.C[1], 5, 1), Note(Pitch.G[1], 5, 1),
+    Note(Pitch.C[1], 5, 1, 1), Note(Pitch.G[1], 5, 1, 1),
+    Note(Pitch.C[1], 5, 1, 1), Note(Pitch.G[1], 5, 1, 1),
 
-    Note(Pitch.A[0], 5, 1), Note(Pitch.E[1], 5, 1),
-    Note(Pitch.A[0], 5, 1), Note(Pitch.E[1], 5, 1),
+    # Note(Pitch.A[0], 5, 1, 1), Note(Pitch.E[1], 5, 1, 1),
+    # Note(Pitch.A[0], 5, 1, 1), Note(Pitch.E[1], 5, 1, 1),
 
-    Note(Pitch.A[0], 5, 1), Note(Pitch.E[1], 5, 1),
-    Note(Pitch.A[0], 5, 1), Note(Pitch.E[1], 5, 1),
+    # Note(Pitch.A[0], 5, 1, 1), Note(Pitch.E[1], 5, 1, 1),
+    # Note(Pitch.A[0], 5, 1, 1), Note(Pitch.E[1], 5, 1, 1),
 
-    Note(Pitch.A[0], 5, 1), Note(Pitch.E[1], 5, 1),
-    Note(Pitch.A[0], 5, 1), Note(Pitch.E[1], 5, 1),
+    # Note(Pitch.A[0], 5, 1, 1), Note(Pitch.E[1], 5, 1, 1),
+    # Note(Pitch.A[0], 5, 1, 1), Note(Pitch.E[1], 5, 1, 1),
 
-    Note(Pitch.C[1], 5, 1), Note(Pitch.G[1], 5, 1),
-    Note(Pitch.C[1], 5, 1), Note(Pitch.G[1], 5, 1),
+    # Note(Pitch.C[1], 5, 1, 1), Note(Pitch.G[1], 5, 1, 1),
+    # Note(Pitch.C[1], 5, 1, 1), Note(Pitch.G[1], 5, 1, 1),
 
-    Note(Pitch.E[1], 5, 1), Note(Pitch.B[1], 5, 1),
-    Note(Pitch.E[1], 5, 1), Note(Pitch.B[1], 5, 1),
+    # Note(Pitch.E[1], 5, 1, 1), Note(Pitch.B[1], 5, 1, 1),
+    # Note(Pitch.E[1], 5, 1, 1), Note(Pitch.B[1], 5, 1, 1),
 
-    Note(Pitch.C[1], 5, 1), Note(Pitch.Ab[1], 5, 1),
-    Note(Pitch.E[1], 5, 1), Note(Pitch.B[1], 5, 1),
+    # Note(Pitch.C[1], 5, 1, 1), Note(Pitch.Ab[1], 5, 1, 1),
+    # Note(Pitch.E[1], 5, 1, 1), Note(Pitch.B[1], 5, 1, 1),
 
-    Note(Pitch.E[1], 5, 1), Note(Pitch.B[1], 5, 1),
-    Note(Pitch.E[1], 5, 1), Note(Pitch.B[1], 5, 1),
+    # Note(Pitch.E[1], 5, 1, 1), Note(Pitch.B[1], 5, 1, 1),
+    # Note(Pitch.E[1], 5, 1, 1), Note(Pitch.B[1], 5, 1, 1),
 
-    Note(Pitch.C[1], 5, 1), Note(Pitch.Ab[1], 5, 1),
-    Note(Pitch.E[1], 5, 1), Note(Pitch.B[1], 5, 1),
+    # Note(Pitch.C[1], 5, 1, 1), Note(Pitch.Ab[1], 5, 1, 1),
+    # Note(Pitch.E[1], 5, 1, 1), Note(Pitch.B[1], 5, 1, 1),
 
-    Note(Pitch.E[1], 5, 1), Note(Pitch.B[1], 5, 1),
-    Note(Pitch.E[1], 5, 1), Note(Pitch.B[1], 5, 1),
+    # Note(Pitch.E[1], 5, 1, 1), Note(Pitch.B[1], 5, 1, 1),
+    # Note(Pitch.E[1], 5, 1, 1), Note(Pitch.B[1], 5, 1, 1),
 
-    Note(Pitch.C[1], 5, 1), Note(Pitch.Ab[1], 5, 1),
-    Note(Pitch.E[1], 5, 1), Note(Pitch.B[1], 5, 1),
+    # Note(Pitch.C[1], 5, 1, 1), Note(Pitch.Ab[1], 5, 1, 1),
+    # Note(Pitch.E[1], 5, 1, 1), Note(Pitch.B[1], 5, 1, 1),
 
-    Note(Pitch.E[1], 5, 1), Note(Pitch.B[1], 5, 1),
-    Note(Pitch.E[1], 5, 1), Note(Pitch.B[1], 5, 1),
+    # Note(Pitch.E[1], 5, 1, 1), Note(Pitch.B[1], 5, 1, 1),
+    # Note(Pitch.E[1], 5, 1, 1), Note(Pitch.B[1], 5, 1, 1),
 
 )
--- micropython/main.py
+++ micropython/main.py
@@ -1,6 +1,4 @@
-from music import Buzzer
-from grieg import IN_THE_HALL_OF_THE_MOUNTAIN_KING_2
-import oak
+import grieg
 
-b = Buzzer(oak.PINS[5])
-b.play_tune(IN_THE_HALL_OF_THE_MOUNTAIN_KING_2)
+
+grieg.main()
