d00e3149b81a75cfef289a403dfafd306ffd8006
Cauê Baasch de Souza <cauebaasch@gmail.com>
2017-10-18 18:03:44 -0200

    Dont activate AP by default

--- micropython/boot.py
+++ micropython/boot.py
@@ -6,7 +6,7 @@ import webrepl
 AP_WLAN = WLAN(AP_IF)
 STA_WLAN = WLAN(STA_IF)
 
-# AP_WLAN.active(True)
+AP_WLAN.active(False)
 # AP_WLAN.config(essid='pyoak2', password='deprecated')
 
 STA_WLAN.active(True)
